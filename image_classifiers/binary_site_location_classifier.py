1cd1c28bbadb6a34ca42ca44e821762d565841f6
MatthewKearns19 <x17492632@student.ncirl.ie>
2021-05-16 18:30:33 +0100

    chrome

--- image_classifiers/binary_site_location_classifier.py
+++ image_classifiers/binary_site_location_classifier.py
@@ -13,6 +13,20 @@ from variables.app_variables import screenshot_results_path, png_file_extension,
 	failed_comparisons_path, pre_defined_screenshot_path, failed_file_extension, artifacts_path
 
 
+chrome_executable_path = '/usr/local/bin/chromedriver.exech'
+pre_defined_screenshot_path = './screenshots/pre_defined_screenshots/'
+screenshot_results_path = './screenshots/browser_screenshot_outputs/'
+failed_comparisons_path = './screenshots/failed_comparisons/'
+failed_file_extension = "_failed_comparison.png"
+png_file_extension = ".png"
+image_quality_distortion_model_path = './models/Sequential_Image_Distortion_Classifier.h5'
+confusion_matrix_assessment_path = './confusion_matrix_assessment/model_classes/'
+assessment_high_resolution_assessment_path = './confusion_matrix_assessment/model_classes/high_resolution/'
+confusion_matrix_output_path = './confusion_matrix_assessment/confusion_matrix_output/'
+matrix_extension = '_confusion_matrix.png'
+artifacts_path = './artifacts/'
+
+
 def compare_page_location_similarity(image_name):
 	test_failed = False
 
--- image_classifiers/image_distortion_classifier.py
+++ image_classifiers/image_distortion_classifier.py
@@ -23,6 +23,20 @@ model = load_model(image_quality_distortion_model_path)
 time.sleep(10)
 
 
+chrome_executable_path = '/usr/local/bin/chromedriver.exech'
+pre_defined_screenshot_path = './screenshots/pre_defined_screenshots/'
+screenshot_results_path = './screenshots/browser_screenshot_outputs/'
+failed_comparisons_path = './screenshots/failed_comparisons/'
+failed_file_extension = "_failed_comparison.png"
+png_file_extension = ".png"
+image_quality_distortion_model_path = './models/Sequential_Image_Distortion_Classifier.h5'
+confusion_matrix_assessment_path = './confusion_matrix_assessment/model_classes/'
+assessment_high_resolution_assessment_path = './confusion_matrix_assessment/model_classes/high_resolution/'
+confusion_matrix_output_path = './confusion_matrix_assessment/confusion_matrix_output/'
+matrix_extension = '_confusion_matrix.png'
+artifacts_path = './artifacts/'
+
+
 def plot_and_save_confusion_matrix(image_name, cm,
                                    classes, title=''):
     plt.imshow(cm, interpolation='nearest', cmap=plt.cm.get_cmap('Blues'))
